5541cbe81ee80c738ac044d24e75cb275c1fdc94
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-04-12 11:36:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-//your JS code here. If required.
+
 const p = document.createElement("p");
-const node = document.createTextNode("DOM load success");
-p.appendChild(node);
+p.innerText="Dom load success";
+// const node = document.createTextNode("DOM load success");
+// p.appendChild(node);
 body.appendChild(p);
